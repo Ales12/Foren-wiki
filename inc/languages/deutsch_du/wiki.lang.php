29d916e82f1b10b239c26887a9a5b6200e6e9d61
Alexandra <38242434+Ales12@users.noreply.github.com>
2022-03-11 14:12:39 +0100

    Update wiki.lang.php

--- inc/languages/deutsch_du/wiki.lang.php
+++ inc/languages/deutsch_du/wiki.lang.php
@@ -18,3 +18,20 @@ $l['wiki_delete'] = "Eintrag ablehnen";
 $l['wiki_accept'] = "Eintrag annehmen";
 $l['delete_wiki'] = "Dein Eintrag ins Foren-Wiki wurde vom Team abgelehnt. Bitte wende dich an das Zuständige Teammitglied, um zu erfahren, weswegen dieser abgelehnt worden ist.";
 $l['accept_wiki'] = "Dein Eintrag ins Foren-Wiki wurde vom Team angenommen. Du findest ihn nun im Foren-Wiki.";
+
+// Kategorien
+$l['newentry_cat'] = "Kategorietitle";
+$l['newentry_cat_desc'] = "Gib hier an, wie die Kategorie heißen soll.";
+$l['newentry_cat_select'] = "Kategorie wählen";
+$l['newentry_linktitle'] = "Linktitel";
+$l['newentry_linktitle_desc'] = "Wie soll der Linkname im Menü lauten?";
+$l['newentry_link'] = "Linkpart";
+$l['newentry_link_desc'] = "Gebe hier an, unter welchen Namen dieser Eintrag im Link oben angezeigt werden soll. Achte darauf, das keine Leerzeichen vorhanden sind.";
+$l['newentry_sort'] = "Sortierung";
+$l['newentry_sort_desc'] = "An welcher Stelle soll der Eintrag/die Kategorie angezeigt werden?";
+$l['newentry_headline'] = "Überschrift des Eintrags";
+$l['newentry_headline_desc'] = "Welche Überschrift soll dieser Wiki-Eintrag haben?";
+$l['newentry_subline'] = "Unterüberschrift";
+$l['newentry_subline_desc'] = "Soll der Eintrag eine Unterüberschrift habe? Wenn nicht notwendig, einfach leer lassen";
+$l['newentry_wikientry'] = "Wiki-Eintrag";
+$l['newentry_wikientry_desc'] = "Hier kannst du nun deinen Eintrag verfassen. Es sind sowohl <b>BBCodes</b> als auch <b>HTML</b> aktiv. Zeilenumbrüche werden Automatisch generiert.";
